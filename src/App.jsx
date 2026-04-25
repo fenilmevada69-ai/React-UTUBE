f8db4e9760a169fc96a94a4c6ddec8d5f1bbdd33
fenilmevada69-ai <fenilmevada69@gmail.com>
2026-04-26 02:21:32 +0530

    first ipdate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx';
+import Card from './components/Card.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Card></Card>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+      <div className="card">
+        <h1>Sarthak Patel</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, blanditiis!</p>
+      </div>
+  )
+}
+
+export default Card
